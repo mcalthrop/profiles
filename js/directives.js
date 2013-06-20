458b40901846d33a09b71ac1b8d7506d994546af
Matt Calthrop <mcalthrop@gmail.com>
2013-06-20 12:12:46 +0100

    Converted page header and footer partials into directives.

--- js/directives.js
+++ js/directives.js
@@ -2,6 +2,28 @@
 
 angular.module('AppDirectives', [])
     .directive(
+    'pageHeader',
+    function factory() {
+        return {
+            restrict: 'E',
+            scope: true,
+            templateUrl: 'inc/directives/page-header.html',
+            link: function () {
+            }
+        };
+    })
+    .directive(
+    'pageFooter',
+    function factory() {
+        return {
+            restrict: 'E',
+            scope: true,
+            templateUrl: 'inc/directives/page-footer.html',
+            link: function () {
+            }
+        };
+    })
+    .directive(
     'mainMenu',
     function factory() {
         return {
