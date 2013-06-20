8ea6ddaa905701587c39a294dc4ff84c914dc6bb
Matt Calthrop <mcalthrop@gmail.com>
2013-06-20 13:46:05 +0100

    Added alt and title attributes to image

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -14,6 +14,20 @@ angular.module('AppControllers', [])
                 $scope.page.id = "profiles";
                 ProfilesDataService.query(
                     function (data) {
+                        // if the alt-text for the image is blank, set it to the first + last name
+                        // and if the title text is not set, set it to the alt text
+                        for (var i = 0, len = data.length ; i < len ; i++) {
+                            var profile = data[i];
+                            var profileName = profile.name;
+                            var profileImage = profile.data.image;
+
+                            if (!profileImage.alt) {
+                                profileImage.alt = profileName.first + " " + profileName.last;
+                            }
+                            if (!profileImage.title) {
+                                profileImage.title = profileImage.alt;
+                            }
+                        }
                         $scope.page.profiles = data;
                     }
                 );
