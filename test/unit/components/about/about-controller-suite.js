4590dc4cf5aa2cc18c94871366c5380cb5b15334
Matt Calthrop <mcalthrop@gmail.com>
2013-06-28 10:31:24 +0100

    First cut of unit test suite for AboutController; incomplete

--- test/unit/components/about/about-controller-suite.js
+++ test/unit/components/about/about-controller-suite.js
@@ -1,15 +1,41 @@
-/* global describe, it, expect */
+/* global describe, beforeEach, module, inject, jasmine, it, expect */
 
-describe('just checking', function () {
+describe('Testing AboutController:', function () {
+    'use strict';
 
-    it('works', function () {
-        expect('something').toNotEqual('nothing');
+    var controllerToTest,
+        scope,
+        mockAboutDataService,
+        mockAboutWrapperService,
+        mockAboutModel,
+        mockPageHeaderModel;
+
+    beforeEach(module('AboutControllerModule'));
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        // TODO: is this the way to do it? ie, mock all the injected parameters?
+        mockAboutDataService = jasmine.createSpyObj('mockAboutDataService', ['query']);
+        mockAboutWrapperService = jasmine.createSpyObj('mockAboutWrapperService', ['query']);
+        mockAboutModel = jasmine.createSpyObj('mockAboutModel', ['setParagraphs', 'getParagraphs']);
+        controllerToTest = $controller('AboutController', {
+            $scope: scope,
+            AboutDataService: mockAboutDataService,
+            AboutWrapperService: mockAboutWrapperService,
+            AboutModel: mockAboutModel,
+            PageHeaderModel: mockPageHeaderModel
+        });
+    }));
+
+    it('should call data service', function () {
+        expect(mockAboutDataService.query).toHaveBeenCalled();
     });
 
-    it('still works', function () {
-        expect('something').toEqual('something');
+    it('should call wrapper service', function () {
+        expect(mockAboutWrapperService.query).toHaveBeenCalled();
     });
 
+    // TODO: more tests - need to make sure correct values have been set on models
 });
 
 /* EOF */
