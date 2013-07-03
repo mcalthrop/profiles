1662edbc617fe6813b6cc2c4d27df21ec55bb6c2
Matt Calthrop <mcalthrop@gmail.com>
2013-07-03 12:43:17 +0100

    Completed unit tests for AboutController

--- test/unit/components/about/about-controller-suite.js
+++ test/unit/components/about/about-controller-suite.js
@@ -9,27 +9,42 @@ describe('Testing AboutController:', function () {
         mockAboutDataService,
         mockAboutDataJson,
         mockAboutWrapperService,
+        mockAboutWrapperJson,
         mockAboutModel,
         mockPageTitleModel,
         mockPageHeaderModel;
 
-    beforeEach(module('AboutControllerModule', 'MockAboutDataJsonModule'));
+    beforeEach(module('AboutControllerModule', 'MockAboutDataJsonModule', 'MockAboutWrapperJsonModule'));
 
-    beforeEach(inject(function (_$controller_, _$rootScope_, _MockAboutDataJson_) {
+    beforeEach(inject(function (_$controller_, _$rootScope_, _MockAboutDataJson_, _MockAboutWrapperJson_) {
         $controller = _$controller_;
 
         $scope = _$rootScope_.$new();
 
         mockAboutDataJson = _MockAboutDataJson_;
-
         mockAboutDataService = jasmine.createSpyObj('mockAboutDataService', ['query']);
         mockAboutDataService.query.andCallFake(function (fn) {
             fn(mockAboutDataJson);
         });
 
+        mockAboutWrapperJson = _MockAboutWrapperJson_;
         mockAboutWrapperService = jasmine.createSpyObj('mockAboutWrapperService', ['query']);
+        mockAboutWrapperService.query.andCallFake(function (fn) {
+            fn(mockAboutWrapperJson);
+        });
 
-        mockAboutModel = jasmine.createSpyObj('mockAboutModel', ['setParagraphs', 'getParagraphs']);
+        mockAboutModel = jasmine.createSpyObj(
+            'mockAboutModel',
+            ['setParagraphs', 'getParagraphs']
+        );
+        mockPageTitleModel = jasmine.createSpyObj(
+            'mockPageTitleModel',
+            ['setTitle', 'getTitle']
+        );
+        mockPageHeaderModel = jasmine.createSpyObj(
+            'mockPageHeaderModel',
+            ['setTitle', 'getTitle', 'setParagraphs', 'getParagraphs']
+        );
 
         controllerToTest = $controller('AboutController', {
             $scope: $scope,
@@ -41,7 +56,11 @@ describe('Testing AboutController:', function () {
         });
     }));
 
-    describe('About data:', function () {
+    it('should set aboutModel correctly on $scope', function () {
+        expect($scope.aboutModel).toBe(mockAboutModel);
+    });
+
+    describe('About data service:', function () {
 
         it('should call data service', function () {
             expect(mockAboutDataService.query).toHaveBeenCalled();
@@ -59,13 +78,27 @@ describe('Testing AboutController:', function () {
             }
         });
 
-        it('should set aboutModel correctly on controller', function () {
-            expect($scope.aboutModel).toBe(mockAboutModel);
+    });
+
+    describe('About wrapper service:', function () {
+
+        it('should call wrapper service', function () {
+            expect(mockAboutWrapperService.query).toHaveBeenCalled();
+        });
+
+        it('should call setTitle() on page title model', function () {
+            expect(mockPageTitleModel.setTitle).toHaveBeenCalled();
+        });
+
+        it('should call setTitle() on page header model', function () {
+            expect(mockPageHeaderModel.setTitle).toHaveBeenCalled();
+        });
+
+        it('should call setParagraphs() on page header model', function () {
+            expect(mockPageHeaderModel.setParagraphs).toHaveBeenCalled();
         });
 
     });
-
-    // TODO: complete tests for About Wrapper data
 });
 
 /* EOF */
